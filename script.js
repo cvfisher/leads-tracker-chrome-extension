af1bb7a5aaac6b86182017a78cd8867b0952146b
cvfisher <cvfisher.93@gmail.com>
2024-10-19 15:06:18 +0100

    render input string to dom

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+}
