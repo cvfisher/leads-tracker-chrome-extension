61ce3a8979df64970bf965201e036680b7b10ffc
cvfisher <cvfisher.93@gmail.com>
2024-10-22 18:02:01 +0100

    implement local storage and refactor render function

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-const myLeads = [];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads(myLeads);
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
   inputEl.value = "";
-  renderLeads();
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
-function renderLeads() {
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-        <a target='_blank' href='${myLeads[i]}'>
-        ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
         </a>
     </li>
     `;
