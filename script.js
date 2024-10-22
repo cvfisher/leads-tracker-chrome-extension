f634a6d20ce401bc67495b41c7f98193324a2936
cvfisher <cvfisher.93@gmail.com>
2024-10-22 19:46:59 +0100

    implement local storage and retrieving url from chrome search bar

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+console.log(chrome);
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads(myLeads);
+  render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
